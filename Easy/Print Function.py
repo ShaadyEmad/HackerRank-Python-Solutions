89b4d684f3a8acd7f903818ab3c81aeb4b7f8641
Shady Emad <95425147+ShaadyEmad@users.noreply.github.com>
2022-08-02 22:58:12 +0200

    Create Print Function.py

---
+++ Easy/Print Function.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    # read variable from user
+    n = int(input())
+    
+    # make loop from (1,n+1)
+    for i in range (1,n+1):
+        # we use (end = '') to print in the same line
+        print(i, end = '')
+    
+    
