d3e2f9704353316ff95dd87aa5797dbf5ee02e77
Shady Emad <95425147+ShaadyEmad@users.noreply.github.com>
2022-08-02 22:43:37 +0200

    Create Loops.py

---
+++ Easy/Loops.py
@@ -0,0 +1,9 @@
+
+if __name__ == '__main__':
+    # read variable from user
+    n = int(input())
+    
+    # make loop from 0 to n
+    for i in range (0,n):
+        # print square of i
+        print(i*i)
