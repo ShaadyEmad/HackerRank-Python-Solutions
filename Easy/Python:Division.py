51106dc4659a1cf389b4d223173305abebb92725
Shady Emad <95425147+ShaadyEmad@users.noreply.github.com>
2022-08-02 22:36:43 +0200

    Create Python:Division.py

---
+++ Easy/Python:Division.py
@@ -0,0 +1,10 @@
+# Read two variables from user
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    # print the integer division
+    print( a // b)
+    
+    # print the float  division
+    print( a / b)
