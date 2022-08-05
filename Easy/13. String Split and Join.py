19f8c9f296ac75a1b37e2a9e8d0b0523c6bcdf14
Shady Emad <95425147+ShaadyEmad@users.noreply.github.com>
2022-08-05 20:41:27 +0200

    Create 13. String Split and Join.py

---
+++ Easy/13. String Split and Join.py
@@ -0,0 +1,14 @@
+# make function called split_and_join() which have parameter (line)
+# paeameter (line) ------> is the input from user 
+def split_and_join(line):
+    # split(" ") ----> every space it split the string 
+    split = line.split(" ")
+    # "-".join   ----> it will replace every space with '-' 
+    return "-".join(split)
+
+if __name__ == '__main__':
+    # takes the input from user
+    line = input()
+    # call the function 
+    result = split_and_join(line)
+    print(result)
